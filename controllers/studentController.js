e7cd5a265eae11bfc337deb71cd2a4bfaf2968d1
Abhay <abhaykhawas@gmail.com>
2026-02-10 23:27:25 +0530

    mongoDB continued

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,10 +3,11 @@ const Student = require('../models/Student')
 
 const createStudent = async (req, res) => {
     try{
-        const { name, age } = req.body
+        const { name, age, course } = req.body
         const student = await Student.create({
             name,
-            age
+            age,
+            course
         })
 
         res.status(201).json(student)
@@ -17,7 +18,77 @@ const createStudent = async (req, res) => {
 }
 
 
+const fetchAllStudent = async (req, res) => {
+    try{
+        const students = await Student.find()
+
+        res.status(200).json({
+            data: students,
+            count: students.length
+        })
+    }
+    catch(err) {
+        res.status(400).json({err: err.message})
+    }
+}
+
+
+const fetchOneStudent = async (req, res) => {
+    try{
+        const id = req.params.id
+        const student = await Student.findById(id)
+
+        if(!student) {
+            return res.status(404).json({message: "Student not found"})
+        }
+
+        res.status(200).json(student)
+
+    }
+    catch(err) {
+        res.status(400).json({err: err.message})
+    }
+}
+
+
+const updateStudent = async (req, res) => {
+    try{
+        const updatedStudent = await Student.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new : true}
+        )
+
+        if(!updatedStudent) {
+            return res.status(404).json({message: "Student not found"})
+        }
+
+        res.status(200).json(updatedStudent)
+    }
+    catch(err){
+        res.status(400).json({err: err.message})
+    }
+}
+
+
+const deleteStudent = async (req, res) => {
+    try{
+        const deletedStudent = await Student.findByIdAndDelete(req.params.id)
+
+        if(!deletedStudent) {
+            res.status(404).json({ message: "Student not found" })
+        }
+
+        res.status(200).json({
+            message: "Student is deleted successfully"
+        })
+    }
+    catch(err) {
+        res.status(400).json({err: err.message})
+    }
+}
+
 
 module.exports = {
-    createStudent
+    createStudent, fetchAllStudent, fetchOneStudent, updateStudent, deleteStudent
 }
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,9 +1,16 @@
-const { createStudent } = require('../controllers/studentController')
+const { createStudent, fetchAllStudent, fetchOneStudent, updateStudent, deleteStudent } = require('../controllers/studentController')
 const express = require('express')
 const router = express.Router()
 
 
 router.post('/', createStudent)
 
+router.get('/', fetchAllStudent)
+
+router.get('/:id', fetchOneStudent)
+
+router.put('/:id', updateStudent)
+
+router.delete('/:id', deleteStudent)
 
 module.exports = router
